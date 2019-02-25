5214445ed59956409503c8441e303b06f2b43399
Botond Dénes <bdenes@scylladb.com>
2019-02-25 21:11:08 +0200

    dev.lua: add and implement required methods
    render_map() still has just a skeleton implementation, it doesn't
    actually render anything. It has just enough to not crash the
    application (a correct, empty return value).

--- surfaces/dev/dev.lua
+++ surfaces/dev/dev.lua
@@ -1,6 +1,31 @@
+function init()
+    GRAPHIC_ASSETS = {}
+
+    local graphic_asset_names = {
+        "terrain_flat_center",
+        "terrain_flat_side",
+        "terrain_hills_center",
+        "terrain_hills_side",
+        "terrain_mountains_center",
+        "terrain_mountains_side"}
+
+    for i, graphic_asset_name in pairs(graphic_asset_names) do
+        GRAPHIC_ASSETS[graphic_asset_name] = WS:get_asset_id_for(graphic_asset_name)
+    end
+end
+
 function render_map_node(mn)
+    gmn = graphic_map_node.new()
+
+    return gmn
 end
 
 function render_map(M)
+    gm = graphic_map.new()
 
+    for i, map_node in pairs(M.map_nodes) do
+        gm.map_nodes[i] = render_map_node(map_node)
+    end
+
+    return gm
 end
